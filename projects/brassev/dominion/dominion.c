629dc56e0400293cddb35eea51485f3d9353c065
Evan Brass <brassev@oregonstate.edu>
2018-10-13 16:49:59 -0700

    Refactor Mine

--- projects/brassev/dominion/dominion.c
+++ projects/brassev/dominion/dominion.c
@@ -746,6 +746,41 @@ void cardEffect_feast(struct gameState *state, int currentPlayer, int choice1) {
 	//Reset Hand
 }
 
+int cardEffect_mine(struct gameState *state, int currentPlayer, int choice1, int choice2, int handPos) {
+	int j = state->hand[currentPlayer][choice1];  //store card we will trash
+
+	if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
+	{
+		return -1;
+	}
+
+	if (choice2 > treasure_map || choice2 < curse)
+	{
+		return -1;
+	}
+
+	if ((getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2))
+	{
+		return -1;
+	}
+
+	gainCard(choice2, state, 2, currentPlayer);
+
+	//discard card from hand
+	discardCard(handPos, currentPlayer, state, 0);
+
+	//discard trashed card
+	for (int i = 0; i < state->handCount[currentPlayer]; i++)
+	{
+		if (state->hand[currentPlayer][i] == j)
+		{
+			discardCard(i, currentPlayer, state, 0);
+			break;
+		}
+	}
+	return 0;
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
 	int i;
@@ -780,38 +815,9 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		return -1;
 
 	case mine:
-		j = state->hand[currentPlayer][choice1];  //store card we will trash
-
-		if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
-		{
+		if (cardEffect_mine(state, currentPlayer, choice1, choice2, handPos) == -1) {
 			return -1;
 		}
-
-		if (choice2 > treasure_map || choice2 < curse)
-		{
-			return -1;
-		}
-
-		if ((getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2))
-		{
-			return -1;
-		}
-
-		gainCard(choice2, state, 2, currentPlayer);
-
-		//discard card from hand
-		discardCard(handPos, currentPlayer, state, 0);
-
-		//discard trashed card
-		for (i = 0; i < state->handCount[currentPlayer]; i++)
-		{
-			if (state->hand[currentPlayer][i] == j)
-			{
-				discardCard(i, currentPlayer, state, 0);
-				break;
-			}
-		}
-
 		return 0;
 
 	case remodel:
